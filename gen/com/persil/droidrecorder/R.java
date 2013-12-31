9788afecb81c6b4184729117069e4692cb3e553d
Emmanuel <ohland_e@epitech.net>
2013-12-31 10:36:21 +0800

    add record view

--- gen/com/persil/droidrecorder/R.java
+++ gen/com/persil/droidrecorder/R.java
@@ -8,6 +8,9 @@
 package com.persil.droidrecorder;
 
 public final class R {
+    public static final class array {
+        public static final int audio_encoder_array=0x7f060000;
+    }
     public static final class attr {
     }
     public static final class dimen {
@@ -24,10 +27,16 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080003;
-        public static final int button1=0x7f080001;
-        public static final int button2=0x7f080002;
-        public static final int textView1=0x7f080000;
+        public static final int LinearLayout1=0x7f090003;
+        public static final int action_settings=0x7f090009;
+        public static final int button1=0x7f090001;
+        public static final int button2=0x7f090002;
+        public static final int chronometer1=0x7f090004;
+        public static final int imageButton1=0x7f090006;
+        public static final int imageButton2=0x7f090007;
+        public static final int linearLayout1=0x7f090005;
+        public static final int spinner1=0x7f090008;
+        public static final int textView1=0x7f090000;
     }
     public static final class layout {
         public static final int activity_bowser_view=0x7f030000;
@@ -35,9 +44,9 @@ public final class R {
         public static final int activity_recorder_view=0x7f030002;
     }
     public static final class menu {
-        public static final int bowser_view=0x7f070000;
-        public static final int main=0x7f070001;
-        public static final int revorder_view=0x7f070002;
+        public static final int bowser_view=0x7f080000;
+        public static final int main=0x7f080001;
+        public static final int revorder_view=0x7f080002;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
@@ -71,10 +80,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
