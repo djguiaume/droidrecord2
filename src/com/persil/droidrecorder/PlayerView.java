28be0ad696c3f45790d1919db85fdab6cc2c6368
djguiaume <guillaume.galan@epitech.eu>
2014-01-03 23:37:47 +0800

    listener completion player + cleaning code

--- src/com/persil/droidrecorder/PlayerView.java
+++ src/com/persil/droidrecorder/PlayerView.java
@@ -27,6 +27,8 @@ import android.view.MenuItem;
 import android.media.AudioManager;
 import android.media.AudioManager.OnAudioFocusChangeListener;
 import android.media.MediaPlayer;
+import android.media.MediaPlayer.OnCompletionListener;
+import android.media.MediaPlayer.OnSeekCompleteListener;
 import android.os.Handler;
 import android.provider.MediaStore;
 import android.view.View;
@@ -40,7 +42,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 import com.persil.droidrecorder2.R;
 
-public class PlayerView extends Activity implements OnSeekBarChangeListener {
+public class PlayerView extends Activity{
 
 	private static String fileName;
 
@@ -57,55 +59,111 @@ public class PlayerView extends Activity implements OnSeekBarChangeListener {
 	private String result;
 	Intent shareIntent;
 	AudioManager am;
+	String basePath;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		am = (AudioManager) getApplicationContext().getSystemService(Context.AUDIO_SERVICE);
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_player_view);
+		basePath = Environment.getExternalStorageDirectory()+File.separator+"DroidRecorder"+File.separator;
 		fileName = getIntent().getExtras().getString("GetFileName");
+		initialize();
+		songName.setText(fileName);
+		mediaPlayer = MediaPlayer.create(this, Uri.fromFile(new File(basePath + fileName)));
+		seekbar.setClickable(true);
+		pauseButton.setEnabled(false);
+		overrideSeekBArListner();
+		initializeShareIntent();
+		mediaPlayer.setOnCompletionListener (new OnCompletionListener()
+		{
+
+			@Override
+			public void onCompletion(MediaPlayer arg0) {
+				onStopThread();
+				//mediaPlayer.seekTo(mediaPlayer.getDuration());
+				pauseButton.setEnabled(false);
+				playButton.setEnabled(true);
+				startTime = 0;
+				seekbar.setProgress((int)startTime);
+				Log.w("stopable", "startTime=0");
+				mediaPlayer.reset();
+				try {
+					mediaPlayer.setDataSource(basePath+fileName);
+					mediaPlayer.prepare();
+				} catch (Exception e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				onStopThread();
+				am.abandonAudioFocus(afChangeListener);
+				
+				
+			}
+			
+		}
+		);
+	}
+
+
+	private void initialize()
+	{
 		songName = (TextView)findViewById(R.id.textView1);
 		startTimeField =(TextView)findViewById(R.id.TextView1);
-		//endTimeField =(TextView)findViewById(R.id.textView2);
 		seekbar = (SeekBar)findViewById(R.id.seekBar1);
 		playButton = (ImageButton)findViewById(R.id.playButton);
 		pauseButton = (ImageButton)findViewById(R.id.pauseButton);
-		songName.setText(fileName);
-
-		mediaPlayer = MediaPlayer.create(this, Uri.fromFile(new File(Environment.getExternalStorageDirectory()+File.separator+"DroidRecorder"+File.separator+fileName)));
 		seekbar.setClickable(true);
-		pauseButton.setEnabled(false);
-		seekbar.setOnSeekBarChangeListener(this);
+		overrideSeekBArListner();
+	}
 
+	private void reinitialize()
+	{
+		initialize();
+		songName.setText(fileName);
+		finalTime = mediaPlayer.getDuration();
+		seekbar.setMax((int) finalTime);
+		boolean isPlaying = mediaPlayer.isPlaying();
+		pauseButton.setEnabled(isPlaying);
+		playButton.setEnabled(!isPlaying);
+	}
+
+	
+	private void initializeShareIntent()
+	{
 		shareIntent = new Intent();
 		shareIntent.setAction(Intent.ACTION_SEND);
 		shareIntent.putExtra("GetFileName",fileName);
 		shareIntent.putExtra(Intent.EXTRA_STREAM,
-				Uri.fromFile(new File(Environment.getExternalStorageDirectory()+File.separator+"DroidRecorder"+File.separator+fileName)));
+				Uri.fromFile(new File(basePath+fileName)));
 		shareIntent.setType("audio/*");
 	}
 
-	public static String getMimeType(String url)
+	// MEDIA PLAYER
+	
+	private void releasMediaPlayer()
 	{
-		String type = null;
-		String extension = MimeTypeMap.getFileExtensionFromUrl(url);
-		if (extension != null) {
-			MimeTypeMap mime = MimeTypeMap.getSingleton();
-			type = mime.getMimeTypeFromExtension(extension);
-		}
-		return type;
+		onStopThread();
+		mediaPlayer.stop();
+		mediaPlayer.release();
+		am.abandonAudioFocus(afChangeListener);
 	}
-
+	
+	private void pauseMediaPlayer()
+	{
+		mediaPlayer.pause();
+		pauseButton.setEnabled(false);
+		playButton.setEnabled(true);
+		am.abandonAudioFocus(afChangeListener);
+	}
+	
 	public void play(View view){
 		int result = am.requestAudioFocus(afChangeListener,
-				// Use the music stream.
 				AudioManager.STREAM_MUSIC,
-				// Request permanent focus.
 				AudioManager.AUDIOFOCUS_GAIN);
-
 		if (result == AudioManager.AUDIOFOCUS_REQUEST_GRANTED) {
 			//am.unregisterMediaButtonEventReceiver(RemoteControlReceiver);
-			
+
 			Toast.makeText(getApplicationContext(), "Playing sound", 
 					Toast.LENGTH_SHORT).show();
 			mediaPlayer.start();
@@ -115,14 +173,7 @@ public class PlayerView extends Activity implements OnSeekBarChangeListener {
 				seekbar.setMax((int) finalTime);
 				oneTimeOnly = 1;
 			} 
-
-			startTimeField.setText(String.format("%d:%d", 
-					TimeUnit.MILLISECONDS.toMinutes((long) startTime),
-					TimeUnit.MILLISECONDS.toSeconds((long) startTime) - 
-					TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.
-							toMinutes((long) startTime)))
-					);
-			seekbar.setProgress((int)startTime);
+			updateSeekbar(startTime);
 			onStartThread(100);
 			pauseButton.setEnabled(true);
 			playButton.setEnabled(false);
@@ -130,67 +181,6 @@ public class PlayerView extends Activity implements OnSeekBarChangeListener {
 		}
 		else 
 			onBackPressed();
-	}
-
-	public void onStopThread() {
-		UpdateSongTime.stop();       
-		mHandler.removeCallbacks(UpdateSongTime);
-	}
-
-	public void onStartThread(long delayMillis) {
-		mHandler.postDelayed(UpdateSongTime, delayMillis);
-	}
-
-
-	private StoppableRunnable UpdateSongTime = new StoppableRunnable() {
-		public void stoppableRun() {
-			startTime = mediaPlayer.getCurrentPosition();
-			if (startTime >= mediaPlayer.getDuration())
-			{
-				mediaPlayer.seekTo(mediaPlayer.getDuration());
-				pauseButton.setEnabled(false);
-				playButton.setEnabled(true);
-				startTime = 0;
-				seekbar.setProgress((int)startTime);
-				Log.w("stopable", "startTime=0");
-				onStopThread();
-			}
-			else{
-				startTimeField.setText(String.format("%d:%d", 
-						TimeUnit.MILLISECONDS.toMinutes((long) startTime),
-						TimeUnit.MILLISECONDS.toSeconds((long) startTime) - 
-						TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.
-								toMinutes((long) startTime))));
-				seekbar.setProgress((int)startTime);
-				onStartThread(100);
-			}
-		}
-	};
-
-	private void reinitialize()
-	{
-		//fileName = getIntent().getExtras().getString("GetFileName");
-		songName = (TextView)findViewById(R.id.textView1);
-		startTimeField =(TextView)findViewById(R.id.TextView1);
-		//endTimeField =(TextView)findViewById(R.id.textView2);
-		seekbar = (SeekBar)findViewById(R.id.seekBar1);
-		playButton = (ImageButton)findViewById(R.id.playButton);
-		pauseButton = (ImageButton)findViewById(R.id.pauseButton);
-		seekbar.setClickable(true);
-		seekbar.setOnSeekBarChangeListener(this);
-		songName.setText(fileName);
-		finalTime = mediaPlayer.getDuration();
-		seekbar.setMax((int) finalTime);
-		if (mediaPlayer.isPlaying())
-		{
-			pauseButton.setEnabled(true);
-			playButton.setEnabled(false);
-		}
-		else
-		{
-			pauseButton.setEnabled(false);
-			playButton.setEnabled(true);
-		}
 	}
 
 	public void pause(View view){
@@ -203,46 +193,82 @@ public class PlayerView extends Activity implements OnSeekBarChangeListener {
 		am.abandonAudioFocus(afChangeListener);
 	}	
 
+	// UPDATER
+	
+	public void onStopThread() {
+		UpdateSongTime.stop();
+		mHandler.removeCallbacks(UpdateSongTime);
+	}
+
+	public void onStartThread(long delayMillis) {
+		mHandler.postDelayed(UpdateSongTime, delayMillis);
+	}
+
+	private StoppableRunnable UpdateSongTime = new StoppableRunnable() {
+		public void stoppableRun() {
+			startTime = mediaPlayer.getCurrentPosition();
+			/*if (startTime >= mediaPlayer.getDuration())
+			{
+				mediaPlayer.seekTo(mediaPlayer.getDuration());
+				pauseButton.setEnabled(false);
+				playButton.setEnabled(true);
+				startTime = 0;
+				seekbar.setProgress((int)startTime);
+				Log.w("stopable", "startTime=0");
+				onStopThread();
+				am.abandonAudioFocus(afChangeListener);
+			}
+			else{*/
+				updateSeekbar(startTime);
+				onStartThread(100);
+			//}
+		}
+	};
+	
+	private void updateSeekbar(double startTime)
+	{
+		startTimeField.setText(String.format("%d:%d", 
+				TimeUnit.MILLISECONDS.toMinutes((long) startTime),
+				TimeUnit.MILLISECONDS.toSeconds((long) startTime) - 
+				TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.
+						toMinutes((long) startTime)))
+				);
+		seekbar.setProgress((int)startTime);
+	}
+
+	// FILE ACTION
+	
 	private void actionRename(String newName)
 	{
-		File basePath = new File(Environment.getExternalStorageDirectory()+File.separator+"DroidRecorder"+File.separator);
+		File oldPath = new File(basePath);
 		String extension = fileName.substring(fileName.lastIndexOf('.'));
-		File from = new File(basePath,fileName);
-		File to = new File(basePath,newName+extension);
+		File from = new File(oldPath,fileName);
+		File to = new File(oldPath,newName+extension);
 		if (from.renameTo(to))
 		{
 			fileName = newName + extension;
 			reinitialize();
 			shareIntent.putExtra(Intent.EXTRA_STREAM,
-					Uri.fromFile(new File(Environment.getExternalStorageDirectory()+File.separator+"DroidRecorder"+File.separator+fileName)));
+					Uri.fromFile(new File(oldPath+fileName)));
 			setShareIntent(shareIntent);
 		}
 	}
 
 	private void showRename()
 	{
-		// get prompts.xml view
 		LayoutInflater li = LayoutInflater.from(context);
 		View promptsView = li.inflate(R.layout.prompt, null);
-
 		AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(context);
-
-		// set prompts.xml to alert dialog builder
 		alertDialogBuilder.setView(promptsView);
 
 		final EditText userInput = (EditText) promptsView.findViewById(R.id.editTextDialogUserInput);
 		userInput.setText(fileName.substring(0, fileName.lastIndexOf('.')));
-
-		// set dialog message
 		alertDialogBuilder
 		.setCancelable(false)
 		.setPositiveButton("OK",
 				new DialogInterface.OnClickListener() {
 			public void onClick(DialogInterface dialog,int id) {
-				// get user input and set it to result
-				// edit text
 				result = userInput.getText().toString();
-				Log.d("popup", userInput.getText().toString());
 				actionRename(result);
 			}
 		})
@@ -252,54 +278,32 @@ public class PlayerView extends Activity implements OnSeekBarChangeListener {
 				dialog.cancel();
 			}
 		});
-
-		// create alert dialog
 		AlertDialog alertDialog = alertDialogBuilder.create();
-
-		// show it
 		alertDialog.show();
 	}
 
 	private void actionDelete()
 	{
-		File file = new File(Environment.getExternalStorageDirectory()+File.separator+"DroidRecorder"+File.separator+fileName);
+		File file = new File(basePath+fileName);
 		file.delete();
 		onBackPressed();
 	}
 
 	private void actionCopy()
 	{
-		File sourceLocation = new File(Environment.getExternalStorageDirectory()+File.separator+"DroidRecorder"+File.separator+fileName);
+		File sourceLocation = new File(basePath+fileName);
 		File destSound = new File(Environment.getExternalStorageDirectory()+File.separator+"media"+File.separator+"ringtones"+File.separator+fileName);
 
 		try {
 			copyFile(sourceLocation, destSound);
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
 	}
 
-	private void copyFile(File sourceLocation, File targetLocation)
-			throws IOException {
-		File directory = targetLocation.getParentFile();
-		if (directory != null && !directory.exists() && !directory.mkdirs()) {
-			throw new IOException("Cannot create dir " + directory.getAbsolutePath());
-		}
-
-		InputStream in = new FileInputStream(sourceLocation);
-		OutputStream out = new FileOutputStream(targetLocation);
-
-		// Copy the bits from instream to outstream
-		byte[] buf = new byte[1024];
-		int len;
-		while ((len = in.read(buf)) > 0) {
-			out.write(buf, 0, len);
-		}
-		in.close();
-		out.close();
-
+	private void setMediaDB(File targetLocation)
+	{
 		ContentValues values = new ContentValues();
 		values.put(MediaStore.MediaColumns.DATA, targetLocation.getAbsolutePath());
 		values.put(MediaStore.MediaColumns.TITLE, fileName);
@@ -311,31 +315,48 @@ public class PlayerView extends Activity implements OnSeekBarChangeListener {
 		values.put(MediaStore.Audio.Media.IS_NOTIFICATION, true);
 		values.put(MediaStore.Audio.Media.IS_ALARM, true);
 		values.put(MediaStore.Audio.Media.IS_MUSIC, false);
-
 		//Insert it into the database
 		Uri uri = MediaStore.Audio.Media.getContentUriForPath(targetLocation.getAbsolutePath());
 		this.getContentResolver().insert(uri, values);
 	}
 
-	@Override
-	public void onProgressChanged(SeekBar seekBar, int progress,
-			boolean fromUser) {
-		// TODO Auto-generated method stub
-
+	private void copyFile(File sourceLocation, File targetLocation)
+			throws IOException {
+		File directory = targetLocation.getParentFile();
+		if (directory != null && !directory.exists() && !directory.mkdirs()) {
+			throw new IOException("Cannot create dir " + directory.getAbsolutePath());
+		}
+		InputStream in = new FileInputStream(sourceLocation);
+		OutputStream out = new FileOutputStream(targetLocation);
+		byte[] buf = new byte[1024];
+		int len;
+		while ((len = in.read(buf)) > 0) {
+			out.write(buf, 0, len);
+		}
+		in.close();
+		out.close();
+		setMediaDB(targetLocation);
 	}
+	
+	public static String getMimeType(String url)
+	{
+		String type = null;
+		String extension = MimeTypeMap.getFileExtensionFromUrl(url);
+		if (extension != null) {
+			MimeTypeMap mime = MimeTypeMap.getSingleton();
+			type = mime.getMimeTypeFromExtension(extension);
+		}
+		return type;
+	}
+
+	// LISTENENER
 
 	@SuppressLint("NewApi") @Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate menu resource file.
 		getMenuInflater().inflate(R.menu.player_view, menu);
-
-		// Locate MenuItem with ShareActionProvider
 		MenuItem item = menu.findItem(R.id.menu_item_share);
-
-		// Fetch and store ShareActionProvider
 		mShareActionProvider = (ShareActionProvider) item.getActionProvider();
 		setShareIntent(shareIntent);
-		// Return true to display menu
 		return true;
 	}
 
@@ -359,7 +380,6 @@ public class PlayerView extends Activity implements OnSeekBarChangeListener {
 
 	private void setShareIntent(Intent shareIntent) {
 		if (mShareActionProvider != null) {
-			Log.w("share", "share action provider pas nul");
 			mShareActionProvider.setShareIntent(shareIntent);
 		}
 	}
@@ -378,57 +398,55 @@ public class PlayerView extends Activity implements OnSeekBarChangeListener {
 
 	@Override
 	public void onBackPressed() {
-		onStopThread();
-		mediaPlayer.pause();
-		mediaPlayer.release();
+		releasMediaPlayer();
 		super.onBackPressed();
 		overridePendingTransition(R.anim.push_right_in, R.anim.push_right_out);
-		am.abandonAudioFocus(afChangeListener);
 	}
 
 	@SuppressLint("NewApi") @Override
 	public boolean	onNavigateUp() {
 		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
-			onStopThread();
-			mediaPlayer.pause();
-			mediaPlayer.release();
+			releasMediaPlayer();
 			boolean ret = super.onNavigateUp();
 			overridePendingTransition(R.anim.push_right_in, R.anim.push_right_out);
-			am.abandonAudioFocus(afChangeListener);
 			return ret;
 		}
 		return false;
 	}
 
-	@Override
-	public void onStartTrackingTouch(SeekBar arg0) {
-		// TODO Auto-generated method stub
 
-	}
+	private void overrideSeekBArListner()
+	{
+		seekbar.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
+			@Override
+			public void onStopTrackingTouch(SeekBar arg0) {
+				// TODO Auto-generated method stub
+				int progress = arg0.getProgress();
+				mediaPlayer.seekTo(progress);
+				startTime = progress;
+			}
 
-	@Override
-	public void onStopTrackingTouch(SeekBar arg0) {
-		// TODO Auto-generated method stub
-		int progress = arg0.getProgress();
-		mediaPlayer.seekTo(progress);
-		startTime = progress;
+			@Override
+			public void onProgressChanged(SeekBar arg0, int arg1, boolean arg2) {
+
+			}
+
+			@Override
+			public void onStartTrackingTouch(SeekBar arg0) {
+
+			}
+		});
 	}
+	
 	OnAudioFocusChangeListener afChangeListener = new OnAudioFocusChangeListener() {
 		public void onAudioFocusChange(int focusChange) {
-			if (focusChange == AudioManager.AUDIOFOCUS_LOSS_TRANSIENT) {
-				mediaPlayer.pause();
-				pauseButton.setEnabled(false);
-				playButton.setEnabled(true);
+			if (focusChange == AudioManager.AUDIOFOCUS_LOSS_TRANSIENT 
+					|| focusChange == AudioManager.AUDIOFOCUS_LOSS) {
+				pauseMediaPlayer();
 			} else if (focusChange == AudioManager.AUDIOFOCUS_GAIN) {
-				mediaPlayer.start();
-				pauseButton.setEnabled(true);
-				playButton.setEnabled(false);
-			} else if (focusChange == AudioManager.AUDIOFOCUS_LOSS) {
-				//am.unregisterMediaButtonEventReceiver(RemoteControlReceiver);
-				mediaPlayer.pause();
-				pauseButton.setEnabled(false);
-				playButton.setEnabled(true);
-				am.abandonAudioFocus(afChangeListener);
+					mediaPlayer.start();
+					pauseButton.setEnabled(true);
+					playButton.setEnabled(false);
 			}
 		}
 	};
