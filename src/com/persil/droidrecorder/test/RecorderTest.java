c507063fb61048ef62a5b2a949593d7555327de1
Flaos <theflaos@gmail.com>
2014-01-04 17:06:21 +0800

    add RecorderTest.java

---
+++ src/com/persil/droidrecorder/test/RecorderTest.java
@@ -0,0 +1,29 @@
+package com.persil.droidrecorder.test;
+
+import java.io.File;
+
+import android.media.MediaRecorder;
+
+import com.persil.droidrecorder.Recorder;
+
+import junit.framework.TestCase;
+
+public class RecorderTest extends TestCase {
+
+	public RecorderTest(String name) {
+		super(name);
+	}
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+	public void testRecorder() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
